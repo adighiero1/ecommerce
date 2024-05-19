d0f0b5fbdc6a3f3fb325d09ea597204dbed8fb0f
Alejandro Dighiero <adighiero1@gmail.com>
2024-05-18 21:17:28 -0400

    fixed routes

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -4,12 +4,12 @@ const passport = require("passport");
 const UserController = require("../controllers/user.controller.js");
 
 const userController = new UserController();
-
+router.get("/admin", passport.authenticate("jwt", { session: false }), userController.isadmin);
 router.post("/register", userController.registerUser);
 router.post("/login", userController.loginUser);
 router.get("/profile", passport.authenticate("jwt", { session: false }), userController.userProfile);
 router.post("/logout", userController.userLogout);
-router.get("/admin", passport.authenticate("jwt", { session: false }), userController.isadmin);
+
 
 module.exports = router;
 

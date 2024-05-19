14ca3140123cb72bb5094129d4076cf8a4e9df72
Alejandro Dighiero <adighiero1@gmail.com>
2024-05-18 21:18:31 -0400

    m

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const ProductController = require("../controllers/product.controller.js");
 const productController = new ProductController(); 
 
+router.put("/:pid", productController.updateProduct);
+router.delete("/:pid", productController.deleteProduct);
 router.get("/", productController.getProducts);
 router.get("/:pid", productController.getProductById);
 router.post("/", productController.addProduct);
-router.put("/:pid", productController.updateProduct);
-router.delete("/:pid", productController.deleteProduct);
+
 
 module.exports = router;
